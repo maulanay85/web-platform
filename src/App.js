7da4da5d88ab8735b47f047422b6c14219ad9e00
Maulana Yusuf S <maulanay85@gmail.com>
2020-12-28 00:08:03 +0700

    add has router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, HashRouter } from 'react-router-dom';
 import Home from './components/pages/Home'
 import PokemonOwned from './components/pages/Pokemon-owned';
 import PokemonDetail from './components/pages/Pokemon-detail';
@@ -8,14 +8,14 @@ import PokemonDetail from './components/pages/Pokemon-detail';
 function App() {
   return (
     <>
-      <Router>
+      <HashRouter>
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/owned' exact component={PokemonOwned} />
           <Route path='/detail/:id' exact component={PokemonDetail} />
         </Switch>
-      </Router>
+      </HashRouter>
 
     </>
   );
